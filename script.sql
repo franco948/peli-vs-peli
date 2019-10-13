079314f9c1bedd0c3625f1b64188289de096ecc2
franco948 <franco.palermo812@gmail.com>
2019-10-13 14:48:26 -0300

    Agregar tabla de competencias

---
+++ script.sql
@@ -0,0 +1,13 @@
+
+CREATE TABLE competencia (
+    id int AUTO_INCREMENT,
+    nombre varchar(100),
+    PRIMARY KEY (id)    
+);
+
+INSERT INTO competencia (nombre) VALUES 
+    ('¿Cual es tu pelicula favorita?'),
+    ('¿Cual es la mas aburrida?'),
+    ('¿Cual tiene los mejores efectos especiales?'),
+    ('¿Que pelicula te gusto mas?'),
+    ('¿Cual tiene el mejor guion?')
