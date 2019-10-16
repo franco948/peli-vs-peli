49dd6f8f03c03d29b87af15c039c0ecabb391834
fpalermo <franco.palermo812@gmail.com>
2019-10-16 20:31:02 -0300

    Agregar tabla para votos

--- script.sql
+++ script.sql
@@ -11,3 +11,12 @@ INSERT INTO competencia (nombre) VALUES
     ('¿Cual tiene los mejores efectos especiales?'),
     ('¿Que pelicula te gusto mas?'),
     ('¿Cual tiene el mejor guion?')
+
+CREATE TABLE voto (
+    id int AUTO_INCREMENT,
+    competencia_id int,
+    pelicula_id int unsigned,
+    PRIMARY KEY (id),
+    FOREIGN KEY (competencia_id) REFERENCES competencia(id),
+    FOREIGN KEY (pelicula_id) REFERENCES pelicula(id)
+);
